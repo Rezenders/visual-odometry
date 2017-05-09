9d39cbef3d165d57ae475814ee600bcaf29e8300
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2017-05-09 18:51:40 -0300

    ✨ Pegando e exibindo as imagens

--- new/src/test.cpp
+++ new/src/test.cpp
@@ -2,7 +2,16 @@
 
 int main(){
   VideoSource cam(480, 640);
+  cv::Mat imBwFull(cam.getSize(), CV_8UC1);
   cv::Mat imBwL, imBwR, imRgbL, imRgbR;
 
-  cam.grabRgbBw(imBwL, imBwR, imRgbL, imRgbR);
+  cv::namedWindow("Video", cv::WINDOW_AUTOSIZE);
+  while (true) {
+    cam.grabRgbBw(imBwL, imBwR, imRgbL, imRgbR);
+    cv::hconcat(imBwL, imBwR, imBwFull);
+    imshow("Video", imBwFull);
+
+    //waits for esc key to exit
+    if(cv::waitKey(30) == 27) break;
+  }
 }
