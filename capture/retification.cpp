9d73ad64d08fc2b4b9c2ab29842e9641270c5699
Gustavo Rezende <gustavorezendesilva@hotmail.com>
2017-05-10 14:58:58 -0300

    🚚 Separando a classe retification em outro arquivo

---
+++ capture/retification.cpp
@@ -0,0 +1,63 @@
+#include "retification.hpp"
+
+retification::retification(){
+  const char* filename = "../../calib/cam_stereo.yml";
+  retification::readParameters(filename);
+}
+
+retification::retification(const char* filename){
+  retification::readParameters(filename);
+}
+
+retification::retification(cv::Size size, const char* filename){
+  retification::setSize(size);
+  retification::readParameters(filename);
+}
+
+void retification::readParameters(){
+  const char* filename = "../../calib/cam_stereo.yml";
+  retification::readParameters(filename);
+}
+
+void retification::readParameters(cv::Size size, const char* filename){
+  retification::setSize(size);
+  retification::readParameters(filename);
+}
+
+void retification::readParameters(const char* filename){
+  cv::FileStorage fs1(filename, cv::FileStorage::READ);
+
+  fs1["K1"] >> K1;
+  fs1["K2"] >> K2;
+  fs1["D1"] >> D1;
+  fs1["D2"] >> D2;
+  fs1["R"] >> R;
+  fs1["T"] >> T;
+
+  fs1["R1"] >> R1;
+  fs1["R2"] >> R2;
+  fs1["P1"] >> P1;
+  fs1["P2"] >> P2;
+  fs1["Q"] >> Q;
+
+  cv::initUndistortRectifyMap(K1, D1, R1, P1, mySize, CV_32F, lmapx, lmapy);
+  cv::initUndistortRectifyMap(K2, D2, R2, P2, mySize, CV_32F, rmapx, rmapy);
+
+  fs1.release();
+}
+
+void retification::rectifyImage(cv::Mat srcL, cv::Mat srcR, cv::Mat &dstL, cv::Mat &dstR){
+  dstL= srcL;
+  dstR= srcR;
+  retification::rectifyImage( dstL, dstR);
+}
+
+void retification::rectifyImage(cv::Mat &imRgbL, cv::Mat &imRgbR){
+  cv::Mat imAuxL, imAuxR;
+
+  cv::remap(imRgbL, imAuxL, lmapx, lmapy, cv::INTER_LINEAR);
+  cv::remap(imRgbR, imAuxR, rmapx, rmapy, cv::INTER_LINEAR);
+
+  imRgbL = imAuxL;
+  imRgbR = imAuxR;
+}
---
+++ capture/retification.hpp
@@ -0,0 +1,44 @@
+#include <opencv2/opencv.hpp>
+
+/**
+* \class retification
+* \brief Classe utilizada para retificar imagens
+*/
+class retification {
+private:
+  cv::Mat R1, R2, P1, P2, Q; //Retification Matrix, Projection Matrix, disparity-to-depth mapping matrix
+	cv::Mat K1, K2, R; // Intrinsics, Rotation
+	cv::Vec3d T; //Translation
+	cv::Mat D1, D2; //distorcion
+  cv::Mat lmapx, lmapy, rmapx, rmapy;
+
+  cv::Size mySize = cv::Size(640, 480);
+public:
+  retification();
+  retification(const char* filename);
+  retification(cv::Size size, const char* filename);
+
+  void readParameters();
+  void readParameters(const char* filename);
+  void readParameters(cv::Size size, const char* filename);
+
+  inline cv::Mat getQ(){return Q;}
+
+  /**
+  * \brief aplica a retificação nas duas imagens passadas como parametro
+  * \param &imRgbL Imagem da esquerda
+  * \param &imRgbR Imagem da direita
+  */
+  void rectifyImage(cv::Mat &imRgbL, cv::Mat &imRgbR);
+  /**
+  * \brief aplica a retificação nas duas ultimas imagens passadas como parametro
+  * \param srcL Imagem de origem da esquerda
+  * \param srcR Imagem de origem da direita
+  * \param &imRgbL Imagem destino da esquerda
+  * \param &imRgbR Imagem destino da direita
+  */
+  void rectifyImage(cv::Mat srcL, cv::Mat srcR, cv::Mat &dstL, cv::Mat &dstR);
+
+
+  inline void setSize(cv::Size size){mySize = size;}
+};
